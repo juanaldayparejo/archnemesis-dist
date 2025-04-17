fixing bug for limb calculation in nemesisfmg

archnemesis/ForwardModel_0.py
@@ -411,9 +411,13 @@ class ForwardModel_0:
                 flagh2p = False
 
                 #Updating the required parameters based on the current geometry
-                self.ScatterX.SOL_ANG = self.MeasurementX.SOL_ANG[0,0]
-                self.ScatterX.EMISS_ANG = self.MeasurementX.EMISS_ANG[0,0]
-                self.ScatterX.AZI_ANG = self.MeasurementX.AZI_ANG[0,0]
+                if self.MeasurementX.EMISS_ANG[0,0]>=0.0:
+                    self.ScatterX.SOL_ANG = self.MeasurementX.SOL_ANG[0,0]
+                    self.ScatterX.EMISS_ANG = self.MeasurementX.EMISS_ANG[0,0]
+                    self.ScatterX.AZI_ANG = self.MeasurementX.AZI_ANG[0,0]
+                else:
+                    self.ScatterX.SOL_ANG = self.MeasurementX.TANHE[0,0]
+                    self.ScatterX.EMISS_ANG = self.MeasurementX.EMISS_ANG[0,0]
 
                 #Changing the different classes taking into account the parameterisations in the state vector
                 xmap = self.subprofretg()
@@ -569,7 +573,6 @@ class ForwardModel_0:
 
         #Interpolating the spectra to the correct altitudes defined in Measurement
         SPECMOD = np.zeros([self.SpectroscopyX.NWAVE,self.MeasurementX.NGEOM])
-        dSPECMOD = np.zeros([self.SpectroscopyX.NWAVE,self.MeasurementX.NGEOM,self.Variables.NX])
         for i in range(self.MeasurementX.NGEOM):
 
             #Find altitudes above and below the actual tangent height
@@ -596,6 +599,7 @@ class ForwardModel_0:
             SPECONV,dSPECONV = self.MeasurementX.convg(self.SpectroscopyX.WAVE,SPECMOD,dSPECMOD,IGEOM='All')
         elif self.SpectroscopyX.ILBL==2:
             SPECONV = self.MeasurementX.lblconv(self.SpectroscopyX.WAVE,SPECMOD,IGEOM='All')
+            dSPECONV = np.zeros([self.MeasurementX.NCONV.max(),self.MeasurementX.NGEOM,self.Variables.NX])
 
         #Applying any changes to the spectra required by the state vector
         SPECONV,dSPECONV = self.subspecret(SPECONV,dSPECONV)
@@ -751,7 +755,7 @@ class ForwardModel_0:
             SPECONV,dSPECONV = self.MeasurementX.lblconvg(self.SpectroscopyX.WAVE,SPECMOD,dSPECMOD,IGEOM='All')
 
         #Calculating the gradients of any parameterisations involving the convolution
-        dSPECONV = self.subspeconv(SPECMOD,dSPECONV)
+        dSPECONV = self.subspeconv(self.SpectroscopyX.WAVE,SPECMOD,dSPECONV)
         
         #Applying any changes to the spectra required by the state vector
         SPECONV,dSPECONV = self.subspecret(SPECONV,dSPECONV)

archnemesis/Measurement_0.py
@@ -1666,30 +1666,30 @@ class Measurement_0:
             if IGEOM=='All':
                 IG = 0
                 if ModSpec.ndim!=2:
-                    raise ValueError('error in lblconvg :: ModSpec must have 2 dimensions (NWAVE,NGEOM)')
+                    raise ValueError('error in lblconv :: ModSpec must have 2 dimensions (NWAVE,NGEOM)')
                 SPECONV = lblconv_ngeom(len(Wave),wavecorr,ModSpec,self.NCONV[IG],self.VCONV[:,IG],self.ISHAPE,self.FWHM)
             else:
                 if ModSpec.ndim!=1:
-                    raise ValueError('error in lblconvg :: ModSpec must have 1 dimensions (NWAVE)')
+                    raise ValueError('error in lblconv :: ModSpec must have 1 dimensions (NWAVE)')
                 IG = IGEOM
                 SPECONV = lblconv(len(Wave),wavecorr,ModSpec,self.NCONV[IG],self.VCONV[:,IG],self.ISHAPE,self.FWHM)
             
         elif self.FWHM<0.0:  #Convolution with VFIL,AFIL
             if IGEOM=='All':
                 if ModSpec.ndim!=2:
-                    raise ValueError('error in lblconvg :: ModSpec must have 2 dimensions (NWAVE,NGEOM)')
+                    raise ValueError('error in lblconv :: ModSpec must have 2 dimensions (NWAVE,NGEOM)')
                 IG = 0
                 SPECONV = lblconv_fil_ngeom(len(Wave),wavecorr,ModSpec,self.NCONV[IG],self.VCONV[:,IG],self.NFIL,self.VFIL,self.AFIL)
             else:
                 if ModSpec.ndim!=1:
-                    raise ValueError('error in lblconvg :: ModSpec must have 1 dimensions (NWAVE)')
+                    raise ValueError('error in lblconv :: ModSpec must have 1 dimensions (NWAVE)')
                 IG = IGEOM
                 SPECONV = lblconv_fil(len(Wave),wavecorr,ModSpec,self.NCONV[IG],self.VCONV[:,IG],self.NFIL,self.VFIL,self.AFIL)
 
         elif self.FWHM==0.0:  #No convolution
             if IGEOM=='All':
                 if ModSpec.ndim!=2:
-                    raise ValueError('error in lblconvg :: ModSpec must have 2 dimensions (NWAVE,NGEOM)')
+                    raise ValueError('error in lblconv :: ModSpec must have 2 dimensions (NWAVE,NGEOM)')
                 SPECONV = np.zeros(self.VCONV.shape)
                 for IG in range(self.NGEOM):
                     SPECONV[:,IG] = np.interp(self.VCONV[:,IG],wavecorr,ModSpec[:,IG])