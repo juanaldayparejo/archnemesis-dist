fixes to absorption coefficient calculation. Addition of CGS constants. Added some helper routines. First pass at getting a GasMixture class to be used for specifying the environment of a spectrally active gas

archnemesis/LineData_0.py
@@ -32,8 +32,10 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 from archnemesis import Data
 from archnemesis import *
 from archnemesis.enums import SpectroscopicLineList, AmbientGas
+import archnemesis.helpers.maths_helper as maths_helper
 import archnemesis.database
 import archnemesis.database.hitran
+from archnemesis.database.datatypes.wave_point import WavePoint
 from archnemesis.database.datatypes.wave_range import WaveRange
 from archnemesis.database.datatypes.gas_isotopes import GasIsotopes
 from archnemesis.database.datatypes.gas_descriptor import RadtranGasDescriptor
@@ -51,6 +53,11 @@ _lgr.setLevel(logging.DEBUG)
 #         per gram, per mole, something like that) the exact unit to be worked out later.
 # * Natural Broadening
 # * Pressure shift
+# * Multiple ambient gasses. At the moment can only have one, but should be able to define
+#   a gas mixture.
+# * Additional lineshapes, have a look at https://www.degruyterbrill.com/document/doi/10.1515/pac-2014-0208/html
+
+
 
 class LineData_0:
     """
@@ -278,7 +285,7 @@ class LineData_0:
         
         """
         
-        doppler_width_const : float  = 1/Data.constants.c_light * np.sqrt(2*np.log(2)*Data.constants.N_avogadro*Data.constants.k_boltzmann)
+        doppler_width_const : float  = 1/Data.constants.c_light_cgs * np.sqrt(2*np.log(2)*Data.constants.N_avogadro*Data.constants.k_boltzmann_cgs)
         
         dws = dict() # result
         
@@ -293,13 +300,13 @@ class LineData_0:
             self, 
             press: float, 
             temp: float,
-            frac: float,
+            amb_frac: float, # fraction of ambient gas
             tref : float = 296,
         ) -> dict[RadtranGasDescriptor, np.ndarray[['NWAVE'], float]]:
         """
         Calculate pressure-broadened width
         """
-        _lgr.debug(f'{press=} {temp=} {frac=} {tref=}')
+        _lgr.debug(f'{press=} {temp=} {amb_frac=} {tref=}')
         
         lws = dict() # result
         
@@ -309,8 +316,8 @@ class LineData_0:
             gas_line_data = self.line_data[gas_desc]
             
             lws[gas_desc] = (
-                gas_line_data.GAMMA_AMB * tratio**gas_line_data.N_AMB * (1-frac) 
-                + gas_line_data.GAMMA_SELF * tratio**gas_line_data.N_SELF * frac
+                gas_line_data.GAMMA_AMB * tratio**gas_line_data.N_AMB * amb_frac 
+                + gas_line_data.GAMMA_SELF * tratio**gas_line_data.N_SELF * (1-amb_frac)
             ) * press
         
         return lws
@@ -354,48 +361,59 @@ class LineData_0:
         qT = self.calculate_partition_sums(T)
         qTref = self.calculate_partition_sums(Tref)
         
+        _lgr.debug(f'{Data.constants.c2_cgs=}')
+        
+        
+        t_factor = Data.constants.c2_cgs * (T - Tref)/(T*Tref)
         
         for i, gas_desc in enumerate(self.gas_isotopes.as_radtran_gasses()):
             gas_line_data = self.line_data[gas_desc]
             
             # Partition function ratio
-            q_ratio = qT[gas_desc] / qTref[gas_desc]
+            q_ratio = qTref[gas_desc] / qT[gas_desc]
             
-            #part = boltzman factor                    * stimulated emission 
-            num   = np.exp(-Data.constants.c2*gas_line_data.ELOWER/T) * ( 1 - np.exp(-Data.constants.c2*gas_line_data.NU/T))
-            den   = np.exp(-Data.constants.c2*gas_line_data.ELOWER/Tref) * ( 1 - np.exp(-Data.constants.c2*gas_line_data.NU/Tref))
+            boltz = np.exp(t_factor*gas_line_data.ELOWER)
             
-            line_strengths[gas_desc] = gas_line_data.SW * q_ratio * num/den
+            stim = ( 1 - np.exp(-Data.constants.c2_cgs*gas_line_data.NU/T)) / ( 1 - np.exp(-Data.constants.c2_cgs*gas_line_data.NU/Tref))
+            
+            line_strengths[gas_desc] = gas_line_data.SW * q_ratio * boltz * stim
+            
+            # Take into account abundances here?
         
         return line_strengths
     
     
     def calculate_absorption(
             self,
-            wavenumbers : np.ndarray,
+            waves : np.ndarray,
             temp: float, 
             press: float,
-            frac: float,
+            amb_frac: float = 1, # fraction of ambient gas
+            wave_unit : ans.enums.WaveUnit = ans.enums.WaveUnit.Wavenumber_cm,
             lineshape_fn: Callable[[np.ndarray, float], np.ndarray] = Data.lineshapes.voigt,
-            wavenumber_window_cutoff: float = 25.0,
+            wavenumber_window_cutoff: float = 25.0, # cm^{-1}
             tref : float = 296,
+            line_strength_cutoff = 1E-32,
         ) -> dict[RadtranGasDescriptor, np.ndarray]:
         """
-        Calculate total absorption coefficient spectrum for wavenumbers.
+        Calculate total absorption coefficient (cm^2) for wavenumbers (cm^{-1}). Returns the value for a single molecule
+        at the specified temperature and pressure.
         
         For details see "applications" section (at bottom) of https://hitran.org/docs/definitions-and-units/
         """
         abs_coeffs = dict()
         
+        waves = WavePoint(waves, wave_unit).to_unit(ans.enums.WaveUnit.Wavenumber_cm).value
+        
         strengths = self.calculate_line_strength(temp, tref)
         alpha_ds = self.calculate_doppler_width(temp)
-        gamma_ls = self.calculate_lorentz_width(press, temp, frac, tref)
+        gamma_ls = self.calculate_lorentz_width(press, temp, amb_frac, tref)
         
         for i, gas_desc in enumerate(self.gas_isotopes.as_radtran_gasses()):
             _lgr.debug(f'Getting absorbtion coefficient for {i}^th gas {gas_desc=}')
             gas_line_data = self.line_data[gas_desc]
         
-            k_total = np.zeros_like(wavenumbers, dtype=float)
+            k_total = np.zeros_like(waves, dtype=float)
             
         
             strength = strengths[gas_desc]
@@ -404,17 +422,20 @@ class LineData_0:
             
             
             for j in range(gas_line_data.NU.size):
-                wn_mask = np.abs(wavenumbers - gas_line_data.NU[j]) < wavenumber_window_cutoff
+                if strength[j] < line_strength_cutoff:
+                    continue
+            
+                wn_mask = np.abs(waves - gas_line_data.NU[j]) < wavenumber_window_cutoff
                 
                 if not np.any(wn_mask):
                     continue
 
-                x = (wavenumbers[wn_mask] - gas_line_data.NU[j]) /  alpha_d[j]
+                x = (waves[wn_mask] - gas_line_data.NU[j]) /  alpha_d[j]
                 y = gamma_l[j] / alpha_d[j]
                 
                 lineshape = lineshape_fn(x,y)
                 
-                k_total[wn_mask] += lineshape * strength[j] / alpha_d[j]
+                k_total[wn_mask] += lineshape * strength[j] / alpha_d[j] # absorption coefficient (cm^2)
                 
                 # Add in continuum absorption here if required
                 

archnemesis/database/datatypes/wave_range.py
@@ -7,10 +7,6 @@ import numpy as np
 import archnemesis as ans
 import archnemesis.enums
 
-import logging
-_lgr = logging.getLogger(__name__)
-_lgr.setLevel(logging.DEBUG)
-
 class WaveRange(NamedTuple):
     
     min : float = 0
@@ -59,7 +55,10 @@ class WaveRange(NamedTuple):
                 result.max = other.max
         return result
     
-    def linspace(self, n : 50, endpoint=True):
+    def linspace(self, n : float = 50, endpoint=True):
         return np.linspace(self.min, self.max, n, endpoint=True)
+    
+    def arange(self, step : float):
+        return np.arange(self.min, self.max, step, endpoint=False)
 
 

archnemesis/database/filetypes/lbltable.py
@@ -8,21 +8,33 @@ import struct
 import numpy as np
 import matplotlib.pyplot as plt
 
+import archnemesis as ans
+from archnemesis.database.datatypes.wave_point import WavePoint
+from archnemesis.database.datatypes.gas_descriptor import RadtranGasDescriptor
+
+import logging
+_lgr = logging.getLogger(__name__)
+_lgr.setLevel(logging.DEBUG)
 
 if TYPE_CHECKING:
     NPRESS = "Number of points in pressure grid"
-    NTEMP_PER_PRESSURE = "Number of temperatures per pressure point"
+    NTEMP_PER_PRESSURE = "Number of temperatures per pressure point, i.e. number of temperature profiles"
     NTEMP = "Number of points in temperature grid"
 
 
 _header_struct = struct.Struct('<2i2f4i')
 
 def _get_body_structs(nwave, npress, ntemp):
-    print(f'{nwave=} {npress=} {ntemp=} {nwave * npress * ntemp=}')
-    return (
-        struct.Struct(f'<{npress}f'),
-        struct.Struct(f'<{ntemp if ntemp > 0 else -ntemp*npress}f'),
-        struct.Struct(f'<{nwave * npress * abs(ntemp)}f'),
+    _lgr.debug(f'{nwave=} {npress=} {ntemp=} {nwave * npress * abs(ntemp)=}')
+    fortran_record_shape = (
+        npress, 
+        ntemp if ntemp > 0 else -ntemp*npress, 
+        nwave * npress * abs(ntemp)
+    )
+    return (fortran_record_shape,
+        struct.Struct(f'<{fortran_record_shape[0]}f'),
+        struct.Struct(f'<{fortran_record_shape[1]}f'),
+        struct.Struct(f'<{fortran_record_shape[2]}f'),
     )
 
 class LblHeader(NamedTuple):
@@ -36,13 +48,19 @@ class LblHeader(NamedTuple):
     iso_id : int
 
 
-class LblDataFormat1(NamedTuple):
-    gas_id : int
-    iso_id : int
-    wave : np.ndarray[['NWAVE'], float]
-    press : np.ndarray[['NPRESS'], float]
-    temp : np.ndarray[['NPRESS', 'NTEMP_PER_PRESSURE'], float]
-    k : np.ndarray[['NWAVE','NPRESS','NTEMP_PER_PRESSURE'], float]
+class LblDataTProfilesAtPressure(NamedTuple):
+    """
+    Line-by-line data specified at `NPRESS` pressures with `NTEMP_PER_PRESSURE` temperature profiles for each pressure.
+    
+    Temperature profiles are associated by index, so the 0th temperature across all pressures is the 0th temperature profile.
+    """
+    gas_id : int # RADTRAN
+    iso_id : int # RADTRAN
+    wave_unit : ans.enums.WaveUnit # ENUM value
+    wave : np.ndarray[['NWAVE'], float] # in `wave_unit`
+    press : np.ndarray[['NPRESS'], float] # bar
+    temp : np.ndarray[['NPRESS', 'NTEMP_PER_PRESSURE'], float] # kelvin
+    k : np.ndarray[['NWAVE','NPRESS','NTEMP_PER_PRESSURE'], float] # 1E20 (cm^2), see function `Spectroscopy_0::write_lbltable` for precident
     
     def write_legacy_header(self, f : str | io.IOBase):
         if not isinstance(f, io.IOBase):
@@ -50,7 +68,7 @@ class LblDataFormat1(NamedTuple):
                 return self.write_legacy_header(g)
         
         f.write(_header_struct.pack(
-            9, # always 9 for some reason
+            9 + self.press.size + self.temp.size, # "fortran record number" at which absorption coefficient data starts
             self.wave.size,
             self.wave[0],
             self.wave[1] - self.wave[0],
@@ -67,7 +85,7 @@ class LblDataFormat1(NamedTuple):
         
         self.write_legacy_header(f)
         
-        p_struct, t_struct, k_struct = _get_body_structs(self.wave.size, self.press.size, self.temp.shape[1])
+        fortran_record_shape, p_struct, t_struct, k_struct = _get_body_structs(self.wave.size, self.press.size, self.temp.shape[1])
         
         f.write(
             p_struct.pack(
@@ -83,11 +101,15 @@ class LblDataFormat1(NamedTuple):
         
         f.write(
             l_struct.pack(
-                self.k
+                self.k # already multiplied by 1E20 to avoid underflow when saving single precision floats
             )
         )
     
-    def plot(self):
+    def plot(self, wave_unit : ans.enums.WaveUnit = ans.enums.WaveUnit.Wavelength_um):
+        """
+        Show a map of `k` (color axis) vs pressure (y-axis) and wave (x-axis) for each temperature 
+        profile. Also plots the temperature profile as a red-line on a shared y-axis.
+        """
         fig, ax = plt.subplots(self.k.shape[2],1, figsize=(12,6*self.k.shape[2]), squeeze=False)
         ax = ax.flatten()
         ax2 = [a.twiny() for a in ax]
@@ -102,9 +124,15 @@ class LblDataFormat1(NamedTuple):
             e[1:-1] = a[:-1]+half_da
             e[-1] = a[-1] + half_da[-1]
             return e
-            
+        
+        wave_points = WavePoint(self.wave, self.wave_unit).to_unit(wave_unit)
+        print(f'{wave_points=}')
+        
         p_edges = np.exp(get_edges(np.log(self.press)))
-        w_edges = get_edges(self.wave)
+        w_edges = get_edges(wave_points.value)
+        
+        fig.suptitle(f'Opacities for {RadtranGasDescriptor(self.gas_id, self.iso_id).label} at each temperature profile.')
+        
         for i in range(self.k.shape[2]):
             ax[i].pcolormesh(
                 w_edges,
@@ -112,7 +140,13 @@ class LblDataFormat1(NamedTuple):
                 #self.k[:,:,i].T
                 np.log(self.k[:,:,i].T)
             )
-            ax[i].set_xlabel('Wavenumber ($cm^{-1}$)')
+            if wave_points.unit == ans.enums.WaveUnit.Wavenumber_cm:
+                ax[i].set_xlabel('Wavenumber ($cm^{-1}$)')
+            elif wave_points.unit == ans.enums.WaveUnit.Wavelength_um:
+                ax[i].set_xlabel(r'Wavelength ($\mu$m)')
+            else:
+                raise RuntimeError(f'Unknown {wave_unit}. Should be one of {ans.enums.WaveUnit.values()}')
+            
             ax[i].set_ylabel('Pressure (atm)')
             ax[i].set_yscale('log')
             ax[i].invert_yaxis()
@@ -121,17 +155,21 @@ class LblDataFormat1(NamedTuple):
             ax2[i].set_xlabel('Temperature (K)')
         
         plt.show()
-            
-        
-        
 
-class LblDataFormat2(NamedTuple):
-    gas_id : int
-    iso_id : int
-    wave : np.ndarray[['NWAVE'], float]
-    press : np.ndarray[['NPRESS'], float]
-    temp : np.ndarray[['NTEMP'], float]
-    k : np.ndarray[['NWAVE','NPRESS','NTEMP'], float]
+
+
+class LblDataTPGrid(NamedTuple):
+    """
+    Line-by-line data specified on a 2-dimensional (`NPRESS`,`NTEMP`) grid. All temperature and pressure
+    combinations on the grid have a `k` entry.
+    """
+    gas_id : int # RADTRAN
+    iso_id : int # RADTRAN
+    wave_unit : ans.enums.WaveUnit # ENUM value
+    wave : np.ndarray[['NWAVE'], float] # wavenumber cm^-1
+    press : np.ndarray[['NPRESS'], float] # bar
+    temp : np.ndarray[['NTEMP'], float] # Kelvin
+    k : np.ndarray[['NWAVE','NPRESS','NTEMP'], float] # 1E20 (cm^2), see function `Spectroscopy_0::write_lbltable` for precident
 
 
     def write_legacy_header(self, f : str | io.IOBase):
@@ -140,7 +178,7 @@ class LblDataFormat2(NamedTuple):
                 return self.write_legacy_header(g)
         
         f.write(_header_struct.pack(
-            9, # always 9 for some reason
+            9 + self.press.size + self.temp.size, # "fortran record number" at which absorption coefficient data starts
             self.wave.size,
             self.wave[0],
             self.wave[1] - self.wave[0],
@@ -157,7 +195,7 @@ class LblDataFormat2(NamedTuple):
         
         self.write_legacy_header(f)
         
-        p_struct, t_struct, k_struct = _get_body_structs(self.wave.size, self.press.size, self.temp.size)
+        fortran_record_shape, p_struct, t_struct, k_struct = _get_body_structs(self.wave.size, self.press.size, self.temp.size)
         
         f.write(
             p_struct.pack(
@@ -173,23 +211,56 @@ class LblDataFormat2(NamedTuple):
         
         f.write(
             l_struct.pack(
-                self.k
+                self.k # already multiplied by 1E20 to avoid underflow when saving single precision floats
             )
         )
 
-    def plot(self):
+    def plot(self, wave_unit : ans.enums.WaveUnit = ans.enums.WaveUnit.Wavelength_um):
+        """
+        Show a map of `k` (color axis) vs pressure (y-axis) and wave (x-axis) for each temperature
+        point on the grid.
+        """
         fig, ax = plt.subplots(self.k.shape[2],1, figsize=(12,6*self.k.shape[2]), squeeze=False)
         ax = ax.flatten()
         
+        wave_points = WavePoint(self.wave, self.wave_unit).to_unit(wave_unit)
+        
+        def get_edges(a):
+            e = np.empty((a.size+1,), dtype=a.dtype)
+            half_da = (a[1:] - a[:-1])/2
+            e[0] = a[0] - half_da[0]
+            e[1:-1] = a[:-1]+half_da
+            e[-1] = a[-1] + half_da[-1]
+            return e
+        
+        p_edges = np.exp(get_edges(np.log(self.press)))
+        w_edges = get_edges(wave_points.value)
+        
+        fig.suptitle(f'Opacities for {RadtranGasDescriptor(self.gas_id, self.iso_id).label} at each point on the temperature grid.')
+        
         for i in range(self.k.shape[2]):
-            ax[i].imshow(np.log(self.k[:,:,i].T), aspect='auto')
+            ax[i].pcolormesh(
+                w_edges,
+                p_edges,
+                #self.k[:,:,i].T
+                np.log(self.k[:,:,i].T)
+            )
+            
+            if wave_points.unit == ans.enums.WaveUnit.Wavenumber_cm:
+                ax[i].set_xlabel('Wavenumber ($cm^{-1}$)')
+            elif wave_points.unit == ans.enums.WaveUnit.Wavelength_um:
+                ax[i].set_xlabel(r'Wavelength ($\mu$m)')
+            else:
+                raise RuntimeError(f'Unknown {wave_unit}. Should be one of {ans.enums.WaveUnit.values()}')
+            
             ax[i].set_xlabel('Wavenumber ($cm^{-1}$)')
             ax[i].set_ylabel('Pressure (atm)')
             ax[i].set_title(f'Temperature: {self.temp[i]} (K)')
         
         plt.show()
 
-    
+
+
 def read_legacy_header(f : str | io.IOBase) -> LblHeader:
     if not isinstance(f, io.IOBase):
         with open(f, 'rb') as g:
@@ -199,12 +270,43 @@ def read_legacy_header(f : str | io.IOBase) -> LblHeader:
     return LblHeader(*_header_struct.unpack_from(buf))
 
 
-def read_legacy(f : str | ioBase) -> LblDataFormat1 | LblDataFormat2:
+def read_legacy(
+        f : str | ioBase, 
+        wave_unit : None | ans.enums.WaveUnit =  None,
+        fortran_record_byte_size = 4, # number of bytes in a fortran record, normally 4
+    ) -> LblDataTProfilesAtPressure | LblDataTPGrid:
+    """
+    Read line-by-line table written in legacy format. NOTE: factor of 1E-20 (reverse of 
+    factor applied when storing to avoid underflow in single precision floats) IS NOT 
+    applied here, it is applied later in ForwardModel_0.
+    
+    ## ARGUMENTS ##
+        
+        f : str | ioBase
+            Filename or FileLike object for table to read.
+        
+        wave_unit : None | ans.enums.WaveUnit
+            Unit of wave data in file,. If None will infer from value of `LblHeader.vmin`; < 100 -> Wavelength_um, >= 100 -> Wavenumber_cm.
+    
+    ## RETURNS ##
+    
+        lbl_data : LblDataTProfilesAtPressure | LblDataTPGrid
+            Line-by-line data in one of the two specified formats depending upon the 
+            format specified in the file.
+    """
     if not isinstance(f, io.IOBase):
         with open(f, 'rb') as g:
-            return read_legacy(g)
+            return read_legacy(g, wave_unit)
     
     hdr = read_legacy_header(f)
+    _lgr.debug(f'{hdr=}')
+    
+    if wave_unit is None:
+        if hdr.vmin < 100:
+            wave_unit = ans.enums.WaveUnit.Wavelength_um
+        else:
+            wave_unit = ans.enums.WaveUnit.Wavenumber_um
+        _lgr.warning(f'No `wave_unit` parameter given to {__name__}.read_legacy(...). Assuming "{wave_unit.name}" based off `LblHeader.vmin` = {hdr.vmin}')
     
     ptk = [
         None,
@@ -212,25 +314,60 @@ def read_legacy(f : str | ioBase) -> LblDataFormat1 | LblDataFormat2:
         None
     ]
     
-    p_struct, t_struct, k_struct = _get_body_structs(hdr.nwave, hdr.npress, hdr.ntemp)
     
+    
+    fortran_record_shape, p_struct, t_struct, k_struct = _get_body_structs(hdr.nwave, hdr.npress, hdr.ntemp)
+    
+    p_shape = (hdr.npress,)
     if hdr.ntemp < 0:
-        
-        for i, (x_struct, x_shape) in enumerate([(p_struct, (hdr.npress,)), (t_struct, (hdr.npress, -hdr.ntemp,)), (k_struct, (hdr.nwave, hdr.npress, -hdr.ntemp))]):
-            print(f'{i=} {x_struct=} {x_shape=}')
-            buf = f.read(x_struct.size)
-            ptk[i] = np.array(x_struct.unpack_from(buf), dtype=float).reshape(x_shape)
-        
-        return LblDataFormat1(hdr.gas_id, hdr.iso_id, np.linspace(hdr.vmin, hdr.vmin+hdr.delv*hdr.nwave, hdr.nwave, endpoint=False), *ptk)
-
+        t_shape = (hdr.npress, -hdr.ntemp)
+        k_shape = (hdr.nwave, hdr.npress, -hdr.ntemp)
+        lbl_data_type = LblDataTProfilesAtPressure
     else:
-        
-        for i, (x_struct, x_shape) in enumerate([(p_struct, (hdr.npress,)), (t_struct, (hdr.ntemp,)), (k_struct, (hdr.nwave, hdr.npress, hdr.ntemp))]):
-            buf = f.read(x_struct.size)
-            ptk[i] = np.array(x_struct.unpack_from(buf), dtype=float).reshape(x_shape)
-        
-        return LblDataFormat2(hdr.gas_id, hdr.iso_id, np.linspace(hdr.vmin, hdr.vmin+hdr.delv*hdr.nwave, hdr.nwave, endpoint=False), *ptk)
+        t_shape = (hdr.ntemp,)
+        k_shape = (hdr.nwave, hdr.npress, hdr.ntemp)
+        lbl_data_type = LblDataTPGrid
+    
+    _lgr.debug(f'{p_shape=} {t_shape=} {k_shape=} {lbl_data_type=}')
+    
+    # Read in pressure and temperature data, these start directly after the header
+    for i, (x_struct, x_shape) in enumerate([(p_struct, p_shape), (t_struct, t_shape)]):
+        _lgr.debug(f'{i=} {x_struct=} {x_shape=}')
+        buf = f.read(x_struct.size)
+        ptk[i] = np.array(x_struct.unpack_from(buf), dtype=float).reshape(x_shape)
+    
+    # Absorption coefficients start at `hdr.irec0` records into the file. THIS IS NOT THE SAME AS AFTER THE PRESSURE AND TEMPERATURE DATA!!
+    current_pos = f.tell()
+    n_bytes_from_start_of_file_to_abs_coeff_data = fortran_record_byte_size * (hdr.irec0-1)
+    
+    if current_pos > n_bytes_from_start_of_file_to_abs_coeff_data:
+        _lgr.warning(f'Header.IREC0 {hdr.irec0} is not correct for this file, attempting to guess the correct location for absorption coefficient data')
+        f.seek(-k_struct.size, 2) # Assume the coefficients end and the end of the file and go backwards to where they should start
+    else:
+        _lgr.debug(f'Skipping to Header.IREC0 {hdr.irec0} bytes into the file as that is where the absorption coefficient data should be. {n_bytes_from_start_of_file_to_abs_coeff_data=}')
+        f.seek(n_bytes_from_start_of_file_to_abs_coeff_data, 0)
+    
+    assert ptk[2] is None, 'Nothing should have been stored here before this point'
+    ptk[2] = np.array(k_struct.unpack_from(f.read(k_struct.size)), dtype=float).reshape(k_shape)
+    
+    # Check we have read all the data correctly
+    current_pos = f.tell()
+    f.seek(0, 2)
+    end_pos = f.tell()
+    
+    if current_pos != end_pos:
+        _lgr.warning(f'We have not read all the data from the file, there are {end_pos - current_pos} bytes remaining. Using an alternate method to try and ensure we get the data correctly.')
+        f.seek(-k_struct.size, 2) # Assume the coefficients end and the end of the file and go backwards to where they should start
+        ptk[2] = np.array(k_struct.unpack_from(f.read(k_struct.size)), dtype=float).reshape(k_shape)
+    
+    return lbl_data_type(hdr.gas_id, hdr.iso_id, wave_unit, np.linspace(hdr.vmin, hdr.vmin+hdr.delv*hdr.nwave, hdr.nwave, endpoint=False), *ptk)
     
     
 
-    
+
+
+
+
+
+
+