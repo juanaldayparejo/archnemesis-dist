Performed some testing between RADTRAN and this code. Voigt profile is correct, 99% sure line calculation is correct. However, saving and loading line-by-line files appears to have issues.

archnemesis/Atmosphere_0.py
@@ -31,6 +31,8 @@ import numpy as np
 from scipy.special import legendre
 import matplotlib.pyplot as plt
 
+import archnemesis.Data.constants as const
+from archnemesis.Data.planet_data import planet_info
 from archnemesis.enums import PlanetEnum, AtmosphericProfileFormatEnum, AtmosphericProfileType
 from archnemesis.helpers import h5py_helper
 
@@ -207,6 +209,13 @@ class Atmosphere_0:
     def AMFORM(self, value):
         self._amform = AtmosphericProfileFormatEnum(value)
 
+    def sanity_check(self):
+        """
+        Some quick tests to make sure that the data in this class has not been corrupted
+        """
+        assert np.all((self.H[-1] - self.H[0]) > 1E4), "The range of heights spanned by the atmosphere should probably be at least 10 km"
+        
+
     def assess(self):
         """
         Assess whether the different variables have the correct dimensions and types
@@ -301,6 +310,8 @@ class Atmosphere_0:
             if self.PARAH2 is not None:
                 assert self.PARAH2.shape == (self.NP,self.NLOCATIONS) , \
                     'PARAH2 must have size (NP,NLOCATIONS)'
+        
+        self.sanity_check()
 
     ##################################################################################
 
@@ -752,9 +763,7 @@ class Atmosphere_0:
         Subroutine to calculate the atmospheric density (kg/m3) at each level
         """
         
-        from archnemesis.Data.gas_data import const
-        
-        R = const["R"]
+        R = const.R
         rho = self.P * self.MOLWT / R / self.T
 
         return rho
@@ -766,9 +775,8 @@ class Atmosphere_0:
         Subroutine to calculate the atmospheric number density (m-3) at each level
         """
         
-        from archnemesis.Data.gas_data import const
         
-        k_B = const["k_B"]
+        k_B = const.k_B
         numdens = self.P / k_B / self.T
 
         return numdens
@@ -805,11 +813,10 @@ class Atmosphere_0:
         of Lindal et al., 1986, Astr. J., 90 (6), 1136-1146
         """
 
-        from archnemesis.Data.gas_data import const
-        from archnemesis.Data.planet_data import planet_info
+        
 
         #Reading data and calculating some parameters
-        Grav = const["G"]
+        Grav = const.G
         data = planet_info[str(int(self.IPLANET))]
         xgm = data["mass"] * Grav * 1.0e24 * 1.0e6
         xomega = 2.*np.pi / (data["rotation"]*24.*3600.)
@@ -882,7 +889,6 @@ class Atmosphere_0:
             
         """
         
-        from archnemesis.Data.gas_data import const
 
         #if self.NLOCATIONS>1:
         #    raise ValueError('error :: adjust_hydrostatP only works if NLOCATIONS = 1')
@@ -899,7 +905,7 @@ class Atmosphere_0:
             self.calc_grav()
 
             #Calculate the scaling factor
-            R = const["R"]
+            R = const.R
             scale = R * self.T / (self.MOLWT * self.GRAV)   #scale height (m)
 
             sh =  0.5*(scale[ialt]+scale[ialt+1])
@@ -942,7 +948,7 @@ class Atmosphere_0:
                 self.calc_grav()
 
                 #Calculate the scaling factor
-                R = const["R"]
+                R = const.R
                 scale = R * self.T[:,iLOC] / (self.MOLWT[:,iLOC] * self.GRAV[:,iLOC])   #scale height (m)
 
                 sh =  0.5*(scale[ialt]+scale[ialt+1])
@@ -975,7 +981,6 @@ class Atmosphere_0:
         Note : Only valid if NLOCATIONS = 1
         """
 
-        from archnemesis.Data.gas_data import const#, gas_info
 
         if self.NLOCATIONS==1:
 
@@ -999,7 +1004,7 @@ class Atmosphere_0:
                 #Calculate the gravity at each altitude level
                 self.calc_grav()
                 #Calculate the scale height
-                R = const["R"]
+                R = const.R
                 scale = R * self.T / (self.MOLWT * self.GRAV)   #scale height (m)
 
                 p[:] = self.P
@@ -1067,7 +1072,7 @@ class Atmosphere_0:
                 self.calc_grav()
 
                 #Calculate the scale height
-                R = const["R"]
+                R = const.R
                 scale = R * self.T / (self.MOLWT * self.GRAV)   #scale height (m)
 
                 if ((ialt>0) & (ialt<self.NP-1)):

archnemesis/Data/lineshapes.py
@@ -1,13 +1,18 @@
 from __future__ import annotations #  for 3.9 compatability
 
 import numpy as np
-from scipy.special import voigt_profile#, wofz, dawsn
+from scipy.special import (
+    voigt_profile, 
+    #wofz, 
+    #dawsn
+)
 
 SQRT_2 = np.sqrt(2)
 SQRT_PI = np.sqrt(np.pi)
 SQRT_2PI = SQRT_2 * SQRT_PI
 SQRT_2log2 = np.sqrt(2*np.log(2))
 SQRT_log2 = np.sqrt(np.log(2))
+
 
 
 def voigt(
@@ -53,17 +58,24 @@ def voigt(
         
         gamma_l : cauchy-lorents HWHM = gamma
     """
-    #sigma_sqrt_2 = alpha_d / SQRT_log2
-    #z = (delta_wn/sigma_sqrt_2) + 1j*(gamma_l/sigma_sqrt_2)
-    #return wofz(z).real / (SQRT_PI * sigma_sqrt_2)
     
-    #sigma = alpha_d / SQRT_2log2
-    #z = (delta_wn + 1j*gamma_l)/(SQRT_2*sigma)
-    #return wofz(z).real / (SQRT_2PI * sigma)
+    # NOTE: Unsure where the log(2) factor comes from but do not get the same answer as existing LBL tables without it.
+    #       it could also be a factor of 1/sqrt(2), they are very similar numbers.
+    #return voigt_profile(delta_wn, alpha_d, np.log(2)*gamma_l) 
+
+    # NOTE: Unsure where the 1/sqrt(2) factor comes from but do not get the same answer as existing LBL tables without it.
+    #       it could also be a factor of log(2), they are very similar numbers.
+    return voigt_profile(delta_wn, alpha_d/SQRT_2, gamma_l/SQRT_2)
+
+
+def voigt_scipy(
+        delta_wn : np.ndarray, 
+        alpha_d : float, 
+        gamma_l : float
+    ) -> np.ndarray:
     
     sigma = alpha_d / SQRT_2log2
     return voigt_profile(delta_wn, sigma, gamma_l)
-
 
 
 def hartmann_empirical_infrared_ch4_h2_broadening(
@@ -76,13 +88,14 @@ def hartmann_empirical_infrared_ch4_h2_broadening(
     wings. Original coefficients recommended by Hartmann (2002)
     """
     chi = np.ones_like(delta_wn)
-    
-    mask_26 = delta_wn < 26
-    mask_60 = delta_wn < 60
+    abs_delta_wn = np.abs(delta_wn)
+    mask_26 = abs_delta_wn < 26
+    mask_60 = abs_delta_wn < 60
     
     mask_a = ~mask_26 & mask_60
     
-    chi[mask_a] = 8.72*np.exp(-np.abs(delta_wn[mask_a])/12.0)
-    chi[~mask_60] = 0.0684*np.exp(-np.abs(delta_wn[~mask_60])/ 393.0)
+    chi[mask_a] = 8.72*np.exp(-abs_delta_wn[mask_a]/12.0)
+    chi[~mask_60] = 0.0684*np.exp(-abs_delta_wn[~mask_60]/ 393.0)
     
-    return chi*voigt(delta_wn, alpha_d, gamma_l)
+    
+    return chi*voigt_scipy(delta_wn, alpha_d, gamma_l)

archnemesis/Models/PreRTModels.py
@@ -9,11 +9,12 @@ import abc
 
 import numpy as np
 import matplotlib.pyplot as plt
-
+from scipy.integrate import simpson
 
 from .ModelBase import ModelBase
 from .ModelParameter import ModelParameter
 
+import archnemesis.Data.constants as const
 from archnemesis.helpers import h5py_helper
 from archnemesis.helpers.maths_helper import ngauss
 from archnemesis.Scatter_0 import kk_new_sub
@@ -1471,8 +1472,7 @@ class Model9(PreRTModelBase):
 
         """
 
-        from scipy.integrate import simpson
-        from archnemesis.Data.gas_data import const
+        
 
 
         if atm_profile_type != AtmosphericProfileType.AEROSOL_DENSITY:
@@ -1482,7 +1482,7 @@ class Model9(PreRTModelBase):
         
         
         #Calculating the actual atmospheric scale height in each level
-        R = const["R"]
+        R = const.R
         scale = R * atm.T / (atm.MOLWT * atm.GRAV)   #scale height (m)
 
         #This gradient is calcualted numerically (in this function) as it is too hard otherwise
@@ -1771,8 +1771,6 @@ class Model32(PreRTModelBase):
         _lgr.debug(f'{frac_scale_height=}')
         _lgr.debug(f'{tau=}')
 
-        #from scipy.integrate import simpson
-        from archnemesis.Data.gas_data import const
         
         if atm_profile_type != AtmosphericProfileType.AEROSOL_DENSITY:
             _msg = f'Model id={cls.id} is only defined for aerosol profiles.'
@@ -1780,7 +1778,7 @@ class Model32(PreRTModelBase):
             raise ValueError(_msg)
 
         #Calculating the actual atmospheric scale height in each level
-        R = const["R"]
+        R = const.R
         scale = R * atm.T / (atm.MOLWT * atm.GRAV)   #scale height (m)
         rho = atm.calc_rho()*1e-3    #density (kg/m3)
 
@@ -2388,11 +2386,10 @@ class Model47(PreRTModelBase):
             _lgr.error(_msg)
             raise ValueError(_msg)
         
-        from archnemesis.Data.gas_data import const
         
         
         # Calculate atmospheric properties
-        R = const["R"]
+        R = const.R
         scale = R * atm.T / (atm.MOLWT * atm.GRAV)   #scale height (m)
         rho = atm.calc_rho()*1e-3    #density (kg/m3)
 

archnemesis/Telluric_0.py
@@ -25,6 +25,7 @@ import os
 import numpy as np
 import h5py
 
+import archnemesis.Data.constants as const
 from archnemesis.enums import PlanetEnum, AtmosphericProfileFormatEnum, SpectraUnit, SpectralCalculationMode
 import archnemesis.helpers.h5py_helper as h5py_helper
 
@@ -195,7 +196,6 @@ class Telluric_0:
         from datetime import datetime, timedelta
         from archnemesis.Data.path_data import archnemesis_path 
         from archnemesis import Atmosphere_0
-        from archnemesis.Data.gas_data import const
         import cdsapi
         
         #Defining the inputs
@@ -301,7 +301,7 @@ class Telluric_0:
         #First estimation of the altitudes
         g0 = 9.80665 #m/s2
         mmol = 0.0289644 #kg/mol
-        R = const["R"]
+        R = const.R
         
         sh = R * temp / (mmol * g0)
         h = -np.log(press/press[0]) * sh

archnemesis/database/datatypes/wave_range.py
@@ -43,7 +43,7 @@ class WaveRange(NamedTuple):
     
     def contains(self, other) -> bool:
         other = other.as_unit(self.unit)
-        return self.min <= other.min and other.max <= self.max
+        return (self.min - other.min) <= 1E-30 and (other.max - self.max) <= 1E-30
     
     def union(self, *others) -> Self:
         vmin, vmax = self.values()
@@ -60,5 +60,4 @@ class WaveRange(NamedTuple):
     
     def arange(self, step : float):
         return np.arange(self.min, self.max, step)
-
 

archnemesis/database/filetypes/lbltable.py
@@ -94,21 +94,21 @@ class LblDataTProfilesAtPressure(NamedTuple):
         print(f'{p_struct=}')
         f.write(
             p_struct.pack(
-                *self.press
+                *(x for x in np.nditer(np.asfortranarray(self.press)))
             )
         )
         
         print(f'{t_struct=}')
         f.write(
             t_struct.pack(
-                *self.temp.flat
+                *(x for x in np.nditer(np.asfortranarray(self.temp)))
             )
         )
         
         print(f'{k_struct=}')
         f.write(
             k_struct.pack(
-                *self.k.flat # already multiplied by 1E20 to avoid underflow when saving single precision floats
+                *(x for x in np.nditer(np.asfortranarray(self.k))) # already multiplied by 1E20 to avoid underflow when saving single precision floats
             )
         )
         
@@ -234,19 +234,19 @@ class LblDataTPGrid(NamedTuple):
         
         f.write(
             p_struct.pack(
-                *self.press.flat
+                *(x for x in np.nditer(np.asfortranarray(self.press)))
             )
         )
         
         f.write(
             t_struct.pack(
-                *self.temp.flat
+                *(x for x in np.nditer(np.asfortranarray(self.temp)))
             )
         )
         
         f.write(
             k_struct.pack(
-                *self.k.flat # already multiplied by 1E20 to avoid underflow when saving single precision floats
+                *(x for x in np.nditer(np.asfortranarray(self.k))) # already multiplied by 1E20 to avoid underflow when saving single precision floats
             )
         )
 
@@ -373,7 +373,7 @@ def read_legacy(
         buf = f.read(x_struct.size)
         _lgr.debug(f'{f.tell()=}')
         
-        ptk[i] = np.array(x_struct.unpack_from(buf), dtype=float).reshape(x_shape)
+        ptk[i] = np.array(x_struct.unpack_from(buf), dtype=float, order='F').reshape(x_shape)
     
     # Absorption coefficients start at `hdr.irec0` records into the file. THIS IS NOT THE SAME AS AFTER THE PRESSURE AND TEMPERATURE DATA!!
     current_pos = f.tell()