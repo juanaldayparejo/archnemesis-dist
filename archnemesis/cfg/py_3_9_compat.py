83abab5d580ab99f3ccc24315be03dbc17ecc714
jackdobinson <jack.dobinson@physics.ox.ac.uk>
2025-09-22 13:43:09 +0100

    trying to get typing annotations working with 3.9 in a way that lets us use types and not turn them all into strings

--- archnemesis/cfg/py_3_9_compat.py
+++ archnemesis/cfg/py_3_9_compat.py
@@ -3,7 +3,15 @@ from __future__ import annotations #  for 3.9 compatability
 Perform compatability setup for python version 3.9
 """
 
-
+import abc
 import typing, typing_extensions
 
 typing.Self = typing_extensions.Self
+
+
+def ABCMeta_or(self, *args):
+    return typing.Union[self, *args]
+
+abc.ABCMeta.__or__ = ABCMeta_or
+
+
