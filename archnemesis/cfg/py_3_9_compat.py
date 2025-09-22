07bdd95563ddf7653c3e3bae7d7ca9e045ffde90
jackdobinson <jack.dobinson@physics.ox.ac.uk>
2025-09-22 14:38:38 +0100

    trying to get typing annotations working with 3.9 in a way that lets us use types and not turn them all into strings

--- archnemesis/cfg/py_3_9_compat.py
+++ archnemesis/cfg/py_3_9_compat.py
@@ -2,17 +2,27 @@ from __future__ import annotations #  for 3.9 compatability
 """
 Perform compatability setup for python version 3.9
 """
+import typing
+from .forbidden_fruit import curse
+
+def UNION_or(self, other):
+    return typing.UNION[self,other]
+
+curse(object, "__or__", UNION_or)
+
 
 import abc
 import typing, typing_extensions
 
+
+
 typing.Self = typing_extensions.Self
 
 
-def UNION_or(self, other):
-    return typing.Union[self, other]
-
-abc.ABCMeta.__or__ = UNION_or
-
-type(None).__or__ = UNION_or
+#def UNION_or(self, other):
+#    return typing.Union[self, other]
+#
+#abc.ABCMeta.__or__ = UNION_or
+#
+#type(None).__or__ = UNION_or
 
