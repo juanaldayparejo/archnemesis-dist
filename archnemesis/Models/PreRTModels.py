6c9c0cd3bdde135f50662aa92d6a9389f118a097
Juan Alday <juanaldayparejo@gmail.com>
2026-04-10 15:54:45 +0200

    adding models 555 and 556

--- archnemesis/Models/PreRTModels.py
+++ archnemesis/Models/PreRTModels.py
@@ -6738,6 +6738,274 @@ class Model500(PreRTModelBase):
         ix = ix + forward_model.Variables.NXVAR[ivar]
 
 
+class Model555(PreRTModelBase):
+    """
+        In this model, we retrieve a correction for the planetary radius
+    """
+    id : int = 555
+
+    def __init__(
+            self, 
+            state_vector_start : int, 
+            #   Index of the state vector where parameters from this model start
+            
+            n_state_vector_entries : int,
+            #   Number of parameters for this model stored in the state vector
+
+        ):
+        """
+            Initialise an instance of the model.
+        """
+        super().__init__(state_vector_start, n_state_vector_entries)
+
+    @classmethod
+    def calculate(cls, Atmosphere, radius_correction, MakePlot=False):
+
+        """
+            FUNCTION NAME : model555()
+
+            DESCRIPTION :
+
+                Function defining the model parameterisation 555 in NEMESIS.
+                In this model, we retrieve a correction for the planetary radius.
+
+            INPUTS :
+
+                Atmosphere :: Atmosphere class
+                radius_correction :: Correction for the planetary radius (km)
+
+            OPTIONAL INPUTS: None
+
+            OUTPUTS :
+                
+                Atmosphere :: Updated Atmosphere class with recomputed pressure levels
+
+            CALLING SEQUENCE:
+
+                Atmosphere = model555(Atmosphere,radius_correction)
+
+            MODIFICATION HISTORY : Juan Alday (15/02/2023)
+
+        """
+
+        _lgr.info(f'Calculating model 555 with radius_correction={radius_correction} km')
+
+        Atmosphere.PLANET_RADIUS = Atmosphere.PLANET_RADIUS + radius_correction*1.0e3
+        Atmosphere.calc_grav()
+
+        return Atmosphere
+
+    @classmethod
+    def from_apr_to_state_vector(
+            cls,
+            variables : "Variables_0",
+            f : IO,
+            varident : np.ndarray[[3],int],
+            varparam : np.ndarray[["mparam"],float],
+            ix : int,
+            lx : np.ndarray[["mx"],int],
+            x0 : np.ndarray[["mx"],float],
+            sx : np.ndarray[["mx","mx"],float],
+            inum : np.ndarray[["mx"],int],
+            npro : int,
+            ngas : int,
+            ndust : int,
+            nlocations : int,
+            runname : str,
+            sxminfac : float,
+        ) -> Self:
+        
+        ix_0 = ix
+        #******** pressure at a given tangent height
+        s = f.readline().split()
+        radius_correction = float(s[0])
+        radius_correction_err = float(s[1])
+
+        x0[ix] = radius_correction
+        lx[ix] = 0
+        inum[ix] = 1
+
+        sx[ix,ix] = (radius_correction_err)**2.
+        #jpre = ix
+    
+        ix = ix + 1
+
+        return cls(ix_0, ix-ix_0)
+
+    @classmethod
+    def from_bookmark(
+            cls,
+            variables : "Variables_0",
+            varident : np.ndarray[[3],int],
+            varparam : np.ndarray[["mparam"],float],
+            ix : int,
+            npro : int,
+            ngas : int,
+            ndust : int,
+            nlocations : int,          
+        ) -> Self:
+        
+        if varident[2] != cls.id:
+            raise ValueError('error in Model555.from_bookmark() :: wrong model id')
+        
+        ix_0 = ix
+        ix = ix + 1
+
+        return cls(ix_0, ix-ix_0)
+
+    def calculate_from_subprofretg(
+            self,
+            forward_model : "ForwardModel_0",
+            ix : int,
+            ipar : int,
+            ivar : int,
+            xmap : np.ndarray,
+        ) -> None:
+        #Model 555. Retrieval of correction to planetary radius
+        #***************************************************************
+
+        radius_correction = forward_model.Variables.XN[ix]
+
+        forward_model.AtmosphereX = self.calculate(forward_model.AtmosphereX,radius_correction)
+
+        ix = ix + forward_model.Variables.NXVAR[ivar]
+
+
+class Model556(PreRTModelBase):
+    """
+        In this model, we retrieve a scaling factor for the planetary radius
+    """
+    id : int = 556
+
+    def __init__(
+            self, 
+            state_vector_start : int, 
+            #   Index of the state vector where parameters from this model start
+            
+            n_state_vector_entries : int,
+            #   Number of parameters for this model stored in the state vector
+
+        ):
+        """
+            Initialise an instance of the model.
+        """
+        super().__init__(state_vector_start, n_state_vector_entries)
+
+    @classmethod
+    def calculate(cls, Atmosphere, radius_scaling_factor, MakePlot=False):
+
+        """
+            FUNCTION NAME : model556()
+
+            DESCRIPTION :
+
+                Function defining the model parameterisation 556 in NEMESIS.
+                In this model, we retrieve a scaling factor for the planetary radius.
+
+            INPUTS :
+
+                Atmosphere :: Atmosphere class
+                radius_scaling_factor :: Scaling factor for the planetary radius
+
+            OPTIONAL INPUTS: None
+
+            OUTPUTS :
+                
+                Atmosphere :: Updated Atmosphere class with recomputed pressure levels
+
+            CALLING SEQUENCE:
+
+                Atmosphere = model556(Atmosphere,radius_scaling_factor)
+
+            MODIFICATION HISTORY : Juan Alday (15/02/2023)
+
+        """
+
+        hpre = Atmosphere.H
+        ppre = Atmosphere.P
+    
+        _lgr.info(f'Calculating model 556 with radius_scaling_factor={radius_scaling_factor}')
+        Atmosphere.PLANET_RADIUS = Atmosphere.PLANET_RADIUS * radius_scaling_factor
+        Atmosphere.calc_grav()
+
+        return Atmosphere
+
+    @classmethod
+    def from_apr_to_state_vector(
+            cls,
+            variables : "Variables_0",
+            f : IO,
+            varident : np.ndarray[[3],int],
+            varparam : np.ndarray[["mparam"],float],
+            ix : int,
+            lx : np.ndarray[["mx"],int],
+            x0 : np.ndarray[["mx"],float],
+            sx : np.ndarray[["mx","mx"],float],
+            inum : np.ndarray[["mx"],int],
+            npro : int,
+            ngas : int,
+            ndust : int,
+            nlocations : int,
+            runname : str,
+            sxminfac : float,
+        ) -> Self:
+        
+        ix_0 = ix
+        #******** pressure at a given tangent height
+        s = f.readline().split()
+        radius_scaling_factor = float(s[0])
+        radius_scaling_factor_err = float(s[1])
+
+        x0[ix] = radius_scaling_factor
+        lx[ix] = 0
+        inum[ix] = 1
+
+        sx[ix,ix] = (radius_scaling_factor_err)**2.
+        #jpre = ix
+    
+        ix = ix + 1
+
+        return cls(ix_0, ix-ix_0)
+
+    @classmethod
+    def from_bookmark(
+            cls,
+            variables : "Variables_0",
+            varident : np.ndarray[[3],int],
+            varparam : np.ndarray[["mparam"],float],
+            ix : int,
+            npro : int,
+            ngas : int,
+            ndust : int,
+            nlocations : int,          
+        ) -> Self:
+        
+        if varident[2] != cls.id:
+            raise ValueError('error in Model556.from_bookmark() :: wrong model id')
+        
+        ix_0 = ix
+        ix = ix + 1
+
+        return cls(ix_0, ix-ix_0)
+
+    def calculate_from_subprofretg(
+            self,
+            forward_model : "ForwardModel_0",
+            ix : int,
+            ipar : int,
+            ivar : int,
+            xmap : np.ndarray,
+        ) -> None:
+        #Model 555. Retrieval of correction to planetary radius
+        #***************************************************************
+
+        radius_scaling_factor = forward_model.Variables.XN[ix]
+
+        forward_model.AtmosphereX = self.calculate(forward_model.AtmosphereX,radius_scaling_factor)
+
+        ix = ix + forward_model.Variables.NXVAR[ivar]
+
+
 class Model666(PreRTModelBase):
     """
         In this model, we retrieve the pressure at a given tangent height.
