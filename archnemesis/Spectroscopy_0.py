added more caching, performed small fixes, fixed polynomial partition function calculation. Added a way to test polynomial partition function w.r.t. tabulated partitoin function. Separated partition function and line data HDF5 file creation routines.

archnemesis/Spectroscopy_0.py
@@ -1447,32 +1447,40 @@ class Spectroscopy_0:
                 lineshape = ans.Data.lineshapes.lorentz
             else:
                 raise ValueError('error in calc_klbl_online :: selected IPROC has not been implemented yet')
-
+            
+            store = np.empty((4, linedata.max_lines_or_bins), dtype=float)
+            k1 = np.empty((k.shape[0],), dtype=float)
             for ipoint in range(npoints):
 
                 p_l = press[ipoint]
                 t_l = temp[ipoint]
 
-                k[:,ipoint,igas] = linedata.calculate_monochromatic_absorption(
-                            waves=wave,             # wavenumbers or wavelengths
-                            temp=t_l,               # kelvin
-                            press=p_l,              # Atmospheres
-                            amb_frac=1.-self_frac_gas,  # fraction of broadening due to ambient gas
-                            wave_unit=self.ISPACE,  # unit of `waves` argument
-                            lineshape_fn=lineshape, # lineshape function to use
-                            line_calculation_wavenumber_window=self.VREL, # cm^{-1}, contribution from lines outside this region should be modelled as continuum absorption (see page 29 of RADTRANS manual).
-                            add_pressure_shift=add_pressure_shift, # whether to include pressure shift in the line positions
+                linedata.add_monochromatic_absorption(
+                    wave_grid=wave,             # wavenumbers or wavelengths
+                    t_calc=t_l,               # kelvin
+                    p_calc=p_l,              # Atmospheres
+                    amb_frac=1.-self_frac_gas,  # fraction of broadening due to ambient gas
+                    wave_unit=self.ISPACE,  # unit of `waves` argument
+                    lineshape_fn=lineshape, # lineshape function to use
+                    wn_calc_window=self.VREL, # cm^{-1}, contribution from lines outside this region should be modelled as continuum absorption (see page 29 of RADTRANS manual).
+                    include_pressure_shift=add_pressure_shift, # whether to include pressure shift in the line positions
+                    
+                    out = k[:,ipoint,igas],
+                    store = store
                 )
 
-                k1 = linedata.calculate_monochromatic_absorption(
-                            waves=wave,             # wavenumbers or wavelengths
-                            temp=t_l+5.,               # kelvin
-                            press=p_l,              # Atmospheres
-                            amb_frac=1.-self_frac_gas,  # fraction of broadening due to ambient gas
-                            wave_unit=self.ISPACE,  # unit of `waves` argument
-                            lineshape_fn=lineshape, # lineshape function to use
-                            line_calculation_wavenumber_window=self.VREL, # cm^{-1}, contribution from lines outside this region should be modelled as continuum absorption (see page 29 of RADTRANS manual).
-                            add_pressure_shift=add_pressure_shift, # whether to include pressure shift in the line positions
+                linedata.add_monochromatic_absorption(
+                    wave_grid=wave,             # wavenumbers or wavelengths
+                    t_calc=t_l+5.,               # kelvin
+                    p_calc=p_l,              # Atmospheres
+                    amb_frac=1.-self_frac_gas,  # fraction of broadening due to ambient gas
+                    wave_unit=self.ISPACE,  # unit of `waves` argument
+                    lineshape_fn=lineshape, # lineshape function to use
+                    wn_calc_window=self.VREL, # cm^{-1}, contribution from lines outside this region should be modelled as continuum absorption (see page 29 of RADTRANS manual).
+                    include_pressure_shift=add_pressure_shift, # whether to include pressure shift in the line positions
+                
+                    out = k1,
+                    store = store,
                 )
 
                 dkdt[:,ipoint,igas] = (k1-k[:,ipoint,igas])/5.

archnemesis/database/filetypes/ans_pseudo_continuum_file.py
@@ -27,7 +27,7 @@ _lgr = logging.getLogger(__name__)
 _lgr.setLevel(logging.DEBUG)
 
 
-
+P_REF_DEFAULT = 1.0 # atmospheres
 
 
 class AnsPseudoContinuumFile(AnsDatabaseFile):
@@ -416,28 +416,46 @@ class AnsPseudoContinuumFile(AnsDatabaseFile):
 					amb_grp = h5py_helper.ensure_grp(b_grp, line_broadener_part.name)
 					broadener_table.to_hdf5(amb_grp)
 	
-	
+	@staticmethod
 	def _get_null_data(
-			self,
 			s_max : float,
 			t_cont : float,
 			p_cont : float,
 			requested_wn_range : tuple[float,float],
-			n_ambient_gasses : int
+			n_ambient_gasses : int,
+			wn_bin_center : None | np.ndarray = None, # (cm^{-1})
+			wn_bin_width : None | float | np.ndarray = 1.0, # (cm^{-1})
 	) -> PseudoContinuumData:
+		"""
+		Return an empty dataset, but it should be ready to accept extra lines if required therefore need to compute a decent value for
+		`wn_bin_center` and `wn_bin_width`.
+		"""
+		if wn_bin_width is not None:
+			if wn_bin_center is None:
+				if isinstance(wn_bin_width, float):
+					wn_bin_center = np.arange(*requested_wn_range, wn_bin_width, dtype=float)
+				else:
+					wn_bin_width_sum = np.cumsum(wn_bin_width)
+					wn_bin_center = np.array([requested_wn_range[0] - wn_bin_width[0]/2, *(requested_wn_range[0] - wn_bin_width[0]/2 + wn_bin_width_sum)], dtype=float)
+			n_bins = len(wn_bin_center)
+		else:
+			n_bins = 0
+			wn_bin_center = np.zeros((n_bins,), dtype=float)
+			wn_bin_width = np.zeros((n_bins,), dtype=float)
+		
 		return PseudoContinuumData(
 			s_max,
 			t_cont,
 			p_cont,
 			requested_wn_range,
-			np.zeros((0,),dtype=float),
-			np.zeros((0,),dtype=float),
-			np.zeros((0,),dtype=float),
-			np.zeros((0,),dtype=float),
-			np.zeros((0,),dtype=float),
-			np.zeros((0,),dtype=float),
-			np.zeros((0,n_ambient_gasses),dtype=float),
-			np.zeros((0,n_ambient_gasses),dtype=float),
+			wn_bin_center,
+			wn_bin_width,
+			np.zeros((n_bins,), dtype=float),
+			np.zeros((n_bins,), dtype=float),
+			np.zeros((n_bins,), dtype=float),
+			np.zeros((n_bins,), dtype=float),
+			np.zeros((n_bins,n_ambient_gasses),dtype=float),
+			np.zeros((n_bins,n_ambient_gasses),dtype=float),
 		)
 	
 	def _get_broadeners_grp(
@@ -522,7 +540,7 @@ class AnsPseudoContinuumFile(AnsDatabaseFile):
 		with self.open('r'):
 			iso_grp = self._get_data_mol_iso_grp(mol_name, local_iso_id, self._file_hdl, on_missing_target, on_missing_mol, on_missing_iso)
 			if iso_grp is None:
-				return self._get_null_data(s_max, temperature, 1, requested_wn_range, n_ambient_gasses)
+				return self._get_null_data(s_max, temperature, P_REF_DEFAULT, requested_wn_range, n_ambient_gasses)
 			
 			result = None
 			
@@ -580,7 +598,7 @@ class AnsPseudoContinuumFile(AnsDatabaseFile):
 			
 			if result is None:
 				_lgr.warn(f'No group found that is compatible with {target_grp_parameters=}, will return empty data. ')
-				return self._get_null_data(s_max, temperature, 1, requested_wn_range, n_ambient_gasses)
+				return self._get_null_data(s_max, temperature, P_REF_DEFAULT, requested_wn_range, n_ambient_gasses)
 			else:
 				return result
 			