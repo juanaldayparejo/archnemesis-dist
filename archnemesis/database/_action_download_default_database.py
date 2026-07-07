71671f11532c7eedb95fc3e74eb7630cf7fad35d
jackdobinson <jack.dobinson@physics.ox.ac.uk>
2026-07-07 17:11:53 +0100

    gitignore additions to support default hitran download.

--- archnemesis/database/_action_download_default_database.py
+++ archnemesis/database/_action_download_default_database.py
@@ -16,9 +16,10 @@ display = lambda *args, **kwargs: print(*args, **kwargs)
 #display = lambda *args, **kwargs: _lgr.info(*args, **kwargs)
 
 
-DEFAULT_DBASE_URL         = "https://digital.csic.es/bitstream/10261/437343/3/hitran24.h5"
-DEFAULT_DBASE_PATH        = Path(archnemesis_resolve_path(archnemesis_path()+'/archnemesis/Data/reference_databases/hitran_2024.h5'))
-DEFAULT_DBASE_SENTRY_PATH = Path(archnemesis_resolve_path(archnemesis_path()+'/archnemesis/Data/reference_databases/hitran_2024.h5.sentry'))
+DEFAULT_DBASE_URL              = "https://digital.csic.es/bitstream/10261/437343/3/hitran24.h5"
+DEFAULT_DBASE_PATH             = Path(archnemesis_resolve_path(archnemesis_path()+'/archnemesis/Data/reference_databases/hitran_2024.h5'))
+DEFAULT_DBASE_SENTRY_PATH      = Path(archnemesis_resolve_path(archnemesis_path()+'/archnemesis/Data/reference_databases/hitran_2024.h5.sentry'))
+DEFAULT_DBASE_NO_DOWNLOAD_PATH = Path(archnemesis_resolve_path(archnemesis_path()+'/archnemesis/Data/reference_databases/hitran_2024.h5.no_download'))
 
 
 def safe_download(url : str, path : Path):
@@ -51,23 +52,29 @@ def download_default_database():
     
     display( 'ARCHNEMESIS :: Setup :: Checking default database...')
 
-    if not DEFAULT_DBASE_SENTRY_PATH.exists():
-        display(f'ARCHNEMESIS :: Setup :: Default database sentry file is not present at {DEFAULT_DBASE_SENTRY_PATH!s}. Therefore will not download.')
-        display(f'ARCHNEMESIS :: Setup :: To enable download, create a dummy file at {DEFAULT_DBASE_SENTRY_PATH!s}.')
-    elif DEFAULT_DBASE_PATH.exists():
-        display(f'ARCHNEMESIS :: Setup :: Default database file at {DEFAULT_DBASE_PATH!s} exists. Therefore will not download again.')
-        display(f'ARCHNEMESIS :: Setup :: To enable download, delete the database file at {DEFAULT_DBASE_PATH!s} and create a dummy file at {DEFAULT_DBASE_SENTRY_PATH!s}.')
-    else:
-        display(f'ARCHNEMESIS :: Setup :: Default database at {DEFAULT_DBASE_PATH!s} has not been downloaded yet and sentry file is present at {DEFAULT_DBASE_SENTRY_PATH!s}. Therefore, downloading file....')
-        
-
+    if DEFAULT_DBASE_SENTRY_PATH.exists() and not DEFAULT_DBASE_PATH.exists() and not DEFAULT_DBASE_NO_DOWNLOAD_PATH.exists():
+        display(f'ARCHNEMESIS :: Setup :: The default database **is not** present at {DEFAULT_DBASE_PATH!s}.')
+        display(f'ARCHNEMESIS :: Setup :: The sentry file **is** present at {DEFAULT_DBASE_SENTRY_PATH!s}.')
+        display(f'ARCHNEMESIS :: Setup :: The stop file **is not** present at {DEFAULT_DBASE_NO_DOWNLOAD_PATH!s}.')
+        display( 'ARCHNEMESIS :: Setup :: Therefore, downloading default database....')
         safe_download(DEFAULT_DBASE_URL, DEFAULT_DBASE_PATH)
-        
-        # If file downloaded successfully, delete the sentry
+    else:
         if DEFAULT_DBASE_PATH.exists():
-            display( 'ARCHNEMESIS :: Setup :: Download completed successfully, deleting sentry file...')
-            DEFAULT_DBASE_SENTRY_PATH.unlink()
-            display( 'ARCHNEMESIS :: Setup :: Sentry file deleted.')
+            display(f'ARCHNEMESIS :: Setup :: The default database **is** present at {DEFAULT_DBASE_PATH!s}.')
+        if not DEFAULT_DBASE_SENTRY_PATH.exists():
+            display(f'ARCHNEMESIS :: Setup :: The sentry file **is not** present at {DEFAULT_DBASE_SENTRY_PATH!s}.')
+        if DEFAULT_DBASE_NO_DOWNLOAD_PATH.exists():
+            display(f'ARCHNEMESIS :: Setup :: The stop file **is** present at {DEFAULT_DBASE_NO_DOWNLOAD_PATH!s}.')
+        
+        display( 'ARCHNEMESIS :: Setup :: Therefore, **not** downloading default database....')
+        display( 'ARCHNEMESIS :: Setup :: ')
+        display( 'ARCHNEMESIS :: Setup :: To enable download perform the following steps:')
+        display( 'ARCHNEMESIS :: Setup :: \t1) Ensure the default database **is not** present at')
+        display(f'ARCHNEMESIS :: Setup :: \t   {DEFAULT_DBASE_PATH!s}')
+        display( 'ARCHNEMESIS :: Setup :: \t2) Ensure the sentry file **is** present at')
+        display(f'ARCHNEMESIS :: Setup :: \t  {DEFAULT_DBASE_SENTRY_PATH!s}')
+        display( 'ARCHNEMESIS :: Setup :: \t3) Ensure the stop file **is not** present at')
+        display(f'ARCHNEMESIS :: Setup :: \t  {DEFAULT_DBASE_NO_DOWNLOAD_PATH!s}')
 
 # Peform action
 download_default_database()
